86c9a13ca40c36f139a54d02d2068dc945d23155
Avishkar Motheelal <avishkar.motheelal11@gmail.com>
2022-10-13 13:09:26 +0200

    Added Security configuration

---
+++ src/main/java/com/example/issueTracker/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.issueTracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests().antMatchers("/register**")
+            .permitAll().anyRequest().authenticated()
+            .and()
+//            .formLogin().loginPage("/login")
+//            .permitAll()
+//            .and()
+            .formLogin()
+            .and()
+            .logout().invalidateHttpSession(true)
+            .clearAuthentication(true).permitAll();
+    }
+}
