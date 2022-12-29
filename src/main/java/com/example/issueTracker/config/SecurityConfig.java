35b34dcfed0d15538bc076efad623dfe1cc84d61
Avishkar Motheelal <avishkar.motheelal11@gmail.com>
2022-12-29 23:01:21 +0200

    Updated security config to remove deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/example/issueTracker/config/SecurityConfig.java
+++ src/main/java/com/example/issueTracker/config/SecurityConfig.java
@@ -1,28 +1,30 @@
 package com.example.issueTracker.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/css/**", "/h2/**","/js/**", "/register**").permitAll()
+            .antMatchers("/css/**", "/h2/**", "/js/**", "/register**").permitAll()
             .anyRequest().authenticated()
             .and()
             .headers()
@@ -37,5 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .logout().invalidateHttpSession(true)
             .clearAuthentication(true).permitAll()
             .logoutUrl("/logout");
+
+        return http.build();
     }
 }
